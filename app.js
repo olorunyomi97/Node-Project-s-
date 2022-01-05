13ac45e5dab4afb8c052afd29b6d556439cf2021
olorunyomi97 <olorunyomi97@gmail.com>
2022-01-05 12:23:13 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks')
+
+
+// MIDDLEWARE
+app.use(express.json())
+
+// ROUTES
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+// app.get('/api/v1/tasks') - get all tasks
+// app.post('/api/v1/tasks') - create new tasks
+// app.get('/api/v1/tasks/:id') - get single task
+// app.patch('/api/v1/tasks/:id') - update single task
+// app.delete('/api/v1/tasks/:id') - delete task
+
+const port = 3500
+app.listen(port,console.log(`Server is listening on port ${port}...`))
---
+++ controllers/tasks.js
@@ -0,0 +1,7 @@
+const getAllTasks = (req, res) => {
+    res.send('all items from the task file')
+}
+
+module.exports = {
+    getAllTasks
+}
