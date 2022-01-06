9a65ac9aea7041b8d87f87e2e592e312a9c09eff
olorunyomi97 <olorunyomi97@gmail.com>
2022-01-07 00:28:06 +0100

    Node Checks and Validations

--- models/Task.js
+++ models/Task.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    name: String,
-    completed: Boolean,
+    // name: String,
+    // completed: Boolean,
     // no_of_bitches: String,
     // category_of_bitches : String
-    // name: {
-    //     type: String,
-    //     required: [true, 'must provide name'],
-    //     trim: true,
-    //     maxlength: [20, 'name can not be more than 20 characters'],
-    //   },
-    //   completed: {
-    //     type: Boolean,
-    //     default: false,
-    // },
+    name: {
+        type: String,
+        required: [true, 'must provide name'],
+        trim: true,
+        maxlength: [20, 'name can not be more than 20 characters'],
+      },
+      completed: {
+        type: Boolean,
+        default: false,
+    },
 })
 
 module.exports = mongoose.model('Task', TaskSchema)
