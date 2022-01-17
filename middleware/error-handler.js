d3d2a2a9dd53fb430518a981b99aa11ee6b368c3
olorunyomi97 <olorunyomi97@gmail.com>
2022-01-18 00:53:55 +0100

    Middle Ware & Error Handlers

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -3,7 +3,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
   }
-  return res.status(500).json({ msg: 'Something went wrong, please try again' })
+  return res.status(500).json({ msg: 'Something went wrong bitch, please try again' })
 }
 
 module.exports = errorHandlerMiddleware
